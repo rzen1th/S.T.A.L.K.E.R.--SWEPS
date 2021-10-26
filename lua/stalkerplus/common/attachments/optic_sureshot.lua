2de61447e46ce9dd420adef69af559839206ff36
Haodong Mo <mohaodong@gmail.com>
2021-10-26 18:10:31 +1100

    Added Sureshot

---
+++ lua/stalkerplus/common/attachments/optic_sureshot.lua
@@ -0,0 +1,23 @@
+ATT.PrintName = "Sightmark Sure Shot Reflex"
+ATT.CompactName = "RDSx1"
+ATT.Icon = Material("entities/stalkerplus_att_optic_sureshot.png", "mips smooth")
+ATT.Description = "Mass produced red dot compact collimator sight."
+ATT.SortOrder = 1
+
+ATT.Model = "models/weapons/stalker/addons/sureshot.mdl"
+
+ATT.Category = "optic_picatinny" // can be "string" or {"list", "of", "strings"}
+
+// Allows a custom sight position to be defined
+ATT.Sights = {
+    {
+        Pos = Vector(0, 10, -1.38985),
+        Ang = Angle(0, 0, 0),
+        Magnification = 1.15
+    }
+}
+
+ATT.HoloSight = true
+ATT.HoloSightReticle = Material("stalkerplus/dot.png", "additive")
+ATT.HoloSightSize = 512
+ATT.HoloSightColor = Color(255, 0, 0)
